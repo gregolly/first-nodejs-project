2068688c95cbdd295eb219686ee677984c6b14f5
gregolly <gregollyff@hotmail.com>
2023-06-20 21:33:27 -0400

    refactoring csvParse to insert data on database

--- src/utils/csvParser.js
+++ src/utils/csvParser.js
@@ -1,6 +1,10 @@
 import fs from "node:fs"
 import { parse } from "csv-parse"
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+import { Database } from "../database.js"
+
+const database = new Database()
 
 const filePath = '/home/gregolly/projetos/nodejs/desafio-01-nodejs/csvFile.csv'
 const apiUrl = 'http://localhost:3333/tasks'
@@ -11,19 +15,24 @@ async function parseCSV() {
 
     for await (const record of parser) {
         const [title, description] = record
-    
-        const data = {
+
+        const task = {
+            id: randomUUID(),
             title,
             description,
+            completed_at: null,
+            created_at: new Date(),
+            updated_at: new Date(),
         }
 
-        console.log(data)
+        console.log(task)
 
         try {
-            const response = await http.request(`${apiUrl}/tasks`, data);
+            const response = await http.request(`${apiUrl}/tasks`, task);
+            database.insert('tasks', data)
             console.log('Requisição enviada com sucesso:', response.data);
         } catch (error) {
-            console.error('Erro ao enviar requisição:', error.message);
+            console.error('Erro ao enviar requisição:', error?.message);
         }
     }
 }
